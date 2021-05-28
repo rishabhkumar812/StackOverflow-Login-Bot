25c93da2e511026e9220c0f16c548a96274d9565
Rishabh Kumar Singh <rishabhkumar812@gmail.com>
2021-05-29 02:36:02 +0530

    Uploading python file.

---
+++ StackOverflowloginBot.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+usernameStr = 'username'
+passwordStr = 'pass'
+
+browser = webdriver.Chrome('D:/SOFTWARES/Chrome Driver/chromedriver.exe')
+browser.get(('https://stackoverflow.com/'))
+
+
+loginBtn = browser.find_element_by_xpath('/html/body/header/div/ol[2]/li[2]/a[1]')
+loginBtn.click()
+
+username = browser.find_element_by_xpath('/html/body/div[3]/div[2]/div/div[3]/form/div[1]/div/input')
+username.send_keys(usernameStr)
+
+password = browser.find_element_by_xpath('/html/body/div[3]/div[2]/div/div[3]/form/div[2]/div[1]/input')
+password.send_keys(passwordStr)
+
+login=browser.find_element_by_xpath('//*[@id="submit-button"]')
+login.click()
+
